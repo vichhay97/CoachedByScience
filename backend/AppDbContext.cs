b864996388ebb77c5d743a977784ce4c2c2120cd
Vichhay <75215104+vichhay97@users.noreply.github.com.>
2026-08-02 11:44:24 +0200

    Add AppDbContext with EF Core Sqlite packages

---
+++ backend/AppDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Exercise> Exercises { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
