093b34e2d26ede080c336680081827873191f9da
Vichhay <75215104+vichhay97@users.noreply.github.com.>
2026-08-02 11:57:04 +0200

    Add initial migration and setup for Exercise entity with SQLite

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data source=coachedbyscience.db"));
 
 var app = builder.Build();
 
