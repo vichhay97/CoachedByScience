e9cf7098f59f3296a3d7317358ac2c78586b659a
Vichhay <75215104+vichhay97@users.noreply.github.com.>
2026-08-02 12:11:26 +0200

    Seed database with initial Exercise records and update /api/exercises endpoint to retrieve from database

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,6 +9,21 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data s
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    if (!db.Exercises.Any())
+    {
+        db.Exercises.AddRange(
+            new Exercise(0, "Preacher Curls", "Preacher curls are a strict isolation exercise for the biceps performed by resting the upper arms on an angled pad while lifting a weight like an EZ-bar or dumbbell."),
+            new Exercise(0, "Converging Machine Chest Press", "A converging chest press machine is a strength training device featuring handles that move forward and inward together during a press. This arc-like motion mimics natural human biomechanics, maximizing chest muscle contraction while reducing shoulder stress."),
+            new Exercise(0, "Pendulum Squat", "The pendulum squat is a guided, machine-based lower-body exercise where your body moves along a curved, swinging arc instead of a straight path. It isolates the quadriceps, glutes, and hamstrings while offering deep range of motion with minimal axial spine load.")
+
+        );
+        db.SaveChanges();
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -36,17 +51,9 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
-app.MapGet("/api/exercises", () =>
+app.MapGet("/api/exercises", (AppDbContext db) =>
 {
-    List<Exercise> exercises = [
-    new Exercise(1, "Preacher Curls", "Preacher curls are a strict isolation exercise for the biceps performed by resting the upper arms on an angled pad while lifting a weight like an EZ-bar or dumbbell."),
-
-    new Exercise(2, "Converging Machine Chest Press", "A converging chest press machine is a strength training device featuring handles that move forward and inward together during a press. This arc-like motion mimics natural human biomechanics, maximizing chest muscle contraction while reducing shoulder stress."),
-
-    new Exercise(3, "Pendulum Squat", "The pendulum squat is a guided, machine-based lower-body exercise where your body moves along a curved, swinging arc instead of a straight path. It isolates the quadriceps, glutes, and hamstrings while offering deep range of motion with minimal axial spine load.")
-    ];
-
-    return exercises;
+    return db.Exercises.ToList();
 })
 .WithName("GetExercises");
 
