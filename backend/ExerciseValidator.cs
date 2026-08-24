105d2f58da93c9ba1417a1e84ca99369242d4af8
Vichhay <75215104+vichhay97@users.noreply.github.com.>
2026-08-24 14:35:08 +0200

    Add ExerciseValidator.IsValid with minimum logic to pass first test

---
+++ backend/ExerciseValidator.cs
@@ -0,0 +1,9 @@
+namespace backend;
+
+public static class ExerciseValidator
+{
+    public static bool IsValid(CreateExerciseRequest request)
+    {
+        return !string.IsNullOrWhiteSpace(request.Name);
+    }
+}
